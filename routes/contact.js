e986e2f5c99c3fb936c91770a27b96a20cd3097d
temit <temitoyosi@gmail.com>
2020-04-15 22:19:32 +0100

    First commit

--- routes/contact.js
+++ routes/contact.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req,res){
-res.render('contact',{
-	title: 'Contact Us'
+	res.render('contact',{
+		title: 'Contact Us'
+	});
 });
-});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -8,25 +8,21 @@ var home = require('./routes/index');
 var contact = require('./routes/contact');
 var elements = require('./routes/elements');
 var include = require('include')(__dirname,'views');
+
 app.set('view engine','ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
-
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/',home);
-app.use('/contact',contact);
-app.use('/elements',elements);
-app.get('/', function(req,res){
-	res.render('index');
-});
-app.get('/contact', function(req,res){
-	res.render('contact');
-});
-app.get('/elements', function(req,res){
-	res.render('elements');
-});
 
-const server = app.listen(7000, () => {
+
+
+app.use('/contact', contact);
+app.use('/elements', elements);
+app.use('/', home);
+
+
+
+const server = app.listen(process.env.PORT||7000, () => {
   console.log(`Express running → PORT ${server.address().port}`);
 });
