c67ae05e1afa1da59c34a47fafb77ae392dbec55
temit <temitoyosi@gmail.com>
2020-05-03 15:06:48 +0100

    nodemailer working

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var express = require('express');
 var router = express.Router();
 var nodemailer = require('nodemailer');
 let argv = require('yargs').argv;
+require('dotenv').config();
 var gplay = require('google-play-scraper'); 
 var result = "";
 
@@ -15,9 +16,9 @@ var reviews = await gplay.reviews({
   sort: gplay.sort.HELPFULNESS
   
 });
-console.log(reviews);
+//console.log(reviews);
 var screenshots = app.screenshots;
-console.log(screenshots);
+//console.log(screenshots);
 
 res.render('index',{
 	title: 'CerviTech | Home',
@@ -28,42 +29,44 @@ res.render('index',{
 
 });
 
-// router.post('/', function(req,res){
-// 	console.log(req.body);
-// 	var name = req.body.name;
-// 	var email = req.body.email;
-// 	var subject = req.body.subject;
-// 	var message = req.body.message;
-//   console.log(name);
-// var transporter = nodemailer.createTransport({
-// 	service:'gmail',
-// 	auth: {
-// 		user:'devs.cervitech@gmail.com',
-// 		pass: 'anelpo14'
-// 	}
+console.log(process.env);
 
-// });
+router.post('/nodemailer', function(req,res){
+	//console.log(req.body);
+	var name = req.body.name;
+	var email = req.body.email;
+	var subject = req.body.subject;
+	var message = req.body.message;
+  //console.log(name);
+var transporter = nodemailer.createTransport({
+	service:'gmail',
+	auth: {
+		user:process.env.GMAIL_USER,
+		pass: process.env.GMAIL_PASS
+	}
+});
 
-// var HelperOptions =
-// {
-// 	from:'devs.cervitech@gmail.com',
-// 	to: 'devs.cervitech@gmail.com',
-// 	subject:email+" - "+subject,
-// 	text:message
-// };
+var HelperOptions =
+{
+	from:process.env.GMAIL_USER,
+	to: process.env.GMAIL_USER,
+	subject: 'New message from contact form at cervitech.com.ng',
+	text:subject+" - "+message +" from "+email+"("+name+")"
+};
 
-// console.log(HelperOptions);
+console.log(HelperOptions);
 
-// transporter.sendMail(HelperOptions,(error,info)=>{
-// 	if(error){
-// 		return console.log(error);
-// 	}
-// 	console.log('successful', info.messageId, info.response);
-// 	console.log(info);
-// });
+transporter.sendMail(HelperOptions,(error,info)=>{
+	if(error){
+		return console.log(error);
+	}
+	console.log('successful', info.messageId, info.response);
+	console.log(info);
+})
 
-// process.env["NODE_TLS_REJECT_UNAUTHORIZED"]=0;
-// });
+
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"]=0;
+});
 
 
 // //POST route from contact us form
